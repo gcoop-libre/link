85ebf810f7ed4a5e4c46c0ab2dc647171419aa46
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2014-04-22 13:46:35 -0500

    Add link_field_formatter_view().

---
+++ link.js
@@ -0,0 +1,21 @@
+/**
+ * Implements hook_field_formatter_view().
+ */
+function link_field_formatter_view(entity_type, entity, field, instance, langcode, items, display) {
+  try {
+    //dpm(items);
+    var element = {};
+    $.each(items, function(delta, item) {
+        var title = item.title ? item.title : item.url
+        element[delta] = {
+          markup: theme(
+            'button_link',
+            { text: title, path: item.url, options: { InAppBrowser: true } }
+          )
+        };
+    });
+    return element;
+  }
+  catch (error) { console.log('link_field_formatter_view - ' + error); }
+}
+
